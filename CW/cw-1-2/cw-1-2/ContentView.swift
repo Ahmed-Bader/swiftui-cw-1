8d456ef7c2f527944720b043d8a017be91937c13
Ahmed the Green <eng.boshhab@gmail.com>
2020-12-24 22:29:50 +0300

    Xcode projects
    added CW-1-1 and CW-1-2
    for class work

---
+++ CW/cw-1-2/cw-1-2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cw-1-2
+//
+//  Created by Ahmed Alkhuder on 24/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
